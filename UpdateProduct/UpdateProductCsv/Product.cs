917c1162453a6ef34e8a850543d9dd5ee6664280
ImNotIsaac03IV <isaac.darko04@outlook.com>
2025-08-25 12:43:04 +0200

    Add UpdateProduct

---
+++ UpdateProduct/UpdateProductCsv/Product.cs
@@ -0,0 +1,78 @@
+﻿/*--------------------------------------------------------------
+*				HTBLA-Leonding / Class: 1xHIF
+*--------------------------------------------------------------
+*              Musterlösung-HA
+*--------------------------------------------------------------
+* Description: UpdateProductCsv
+*--------------------------------------------------------------
+*/
+
+namespace UpdateProductCsv
+{
+    using System.IO;
+    using System.Globalization;
+    using System;
+
+    public class Product
+    {
+        public static string CreateCsvLine(string productCodes, string descriptions, string taxClasses, decimal retails)
+        {
+            return $"{productCodes};{descriptions};{taxClasses};{retails.ToString(CultureInfo.InvariantCulture)}";
+        }
+        public static bool TryParseLine(string line, out string code, out string descriptions, out string taxClasses, out decimal retails)
+        {
+            code = "";
+            descriptions = "";
+            taxClasses = "";
+            retails = 0;
+
+            string[] parts = line.Split(';');
+
+            if (parts.Length == 4)
+            {
+                code = parts[0];
+                descriptions = parts[1];
+                taxClasses = parts[2];
+                retails = decimal.Parse(parts[3]);
+
+                return true;
+            }
+
+            return false;
+        }
+        public static bool IsValidArgLength(string[] argLength)
+        {
+            if (argLength.Length == 2)
+            {
+                return true;
+            }
+
+            Console.WriteLine("Usage: UpdateProductCsv <filename.csv> <percentage>");
+            return false;
+        }
+        public static bool IsValidFileName(string fileName)
+        {
+            if (File.Exists(fileName) && fileName.EndsWith(".csv"))
+            {
+                return true;
+            }
+            Console.WriteLine("Error: File must be a .csv and must exist.");
+            return false;
+        }
+        public static void CreateBackup(string fileName, string[] updatedList)
+        {
+            string tempFile = Path.ChangeExtension(fileName, ".$$$");
+            File.WriteAllLines(tempFile, updatedList);
+
+            string backup = Path.ChangeExtension(fileName, ".bak");
+
+            if (File.Exists(backup))
+            {
+                File.Delete(backup);
+            }
+
+            File.Move(fileName, backup);
+            File.Move(tempFile, fileName);
+        }
+    }
+}
---
+++ UpdateProduct/UpdateProductCsv/UpdateProductCsv.cs
@@ -0,0 +1,87 @@
+﻿/*--------------------------------------------------------------
+*				HTBLA-Leonding / Class: 1xHIF
+*--------------------------------------------------------------
+*              Musterlösung-HA
+*--------------------------------------------------------------
+* Description: UpdateProductCsv
+*--------------------------------------------------------------
+*/
+
+namespace UpdateProductCsv
+{
+    using System;
+    using System.Globalization;
+    using System.IO;
+
+    public class UpdateProductCsv
+    {
+        public static int Main(string[] args)
+        {
+            if (!Product.IsValidArgLength(args))
+            {
+                return 1;
+            }
+
+            string fileName = args[0];
+
+            if (!Product.IsValidFileName(fileName))
+            {
+                return 1;
+            }
+
+            if (!decimal.TryParse(args[1], NumberStyles.Any, CultureInfo.InvariantCulture, out decimal percent))
+            {
+                Console.WriteLine("Error: Invalid percentage. Use a dot for decimal (e.g., 10.5).");
+                return 1;
+            }
+
+            string[] lines = File.ReadAllLines(fileName);
+
+            string[] productCodes = new string[lines.Length];
+            string[] descriptions = new string[lines.Length];
+            string[] taxClasses = new string[lines.Length];
+            decimal[] retails = new decimal[lines.Length];
+
+            int count = 0;
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string line = lines[i];
+
+                if (Product.TryParseLine(line, out string parsedCodes, out string parsedDescrptions, out string parsedTaxClasses, out decimal parsedRetails))
+                {
+                    productCodes[count] = parsedCodes;
+                    descriptions[count] = parsedDescrptions;
+                    taxClasses[count] = parsedTaxClasses;
+                    retails[count] = parsedRetails;
+
+                    count++;
+                }
+                else
+                {
+                    Console.WriteLine($"Invalid Format detected: {line}");
+                }
+            }
+
+            for (int i = 0; i < retails.Length; i++)
+            {
+                retails[i] = Math.Round(retails[i] * (1 + percent / 100), 2);
+            }
+
+            string[] updatedLines = new string[count + 1];
+
+            updatedLines[0] = lines[0];
+
+            for (int i = 0; i < count; i++)
+            {
+                updatedLines[i + 1] = Product.CreateCsvLine(productCodes[i], descriptions[i], taxClasses[i], retails[i]);
+            }
+
+            Product.CreateBackup(fileName, updatedLines);
+
+            Console.WriteLine("Prices updated and file saved.");
+
+            return 0;
+        }
+    }
+}
